7b78b8ad12c73d8bab2abf682489ea75f2e8d258
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-11 19:10:45 -0700

    Using alerts to find my errors
    Using alerts to find my errors

--- Birchard_Eirc_Expressions_Assignment/js/cal.js
+++ Birchard_Eirc_Expressions_Assignment/js/cal.js
@@ -14,11 +14,14 @@
 
 //TODO: declare variables
 
+
 var startingGallons      //number variable
 var milesDriven          //number variable
 var vehicleType          //sting variable
 var isThereGasLeftOver   //boolean variable
 var array                //array variable for console output consolidation
+var fillUp               //numerical variable for filling up tank in gallons
+var MPG                  //variable for miles per gallon
 
 //2 examples of arithmetic operators + and -, /, *
 //example of an array used as part of the calculation
@@ -30,25 +33,43 @@ var array                //array variable for console output consolidation
 //TODO: prompts
 //data types number, string, boolean, array
 
-vehicleType = prompt("What type of vehicle do you drive?"); //string input from user
+vehicleType = prompt("What type of vehicle do you drive?");          //prompt for string input from user
 
-startingGallons = prompt("Starting fuel in tank"); //number input from user
+startingGallons = prompt("Starting fuel in tank");                   //prompt for number input from user
 
-milesDriven = prompt ("How many miles have you driven so far")
+milesDriven = prompt ("How many miles have you driven so far")       //prompt for number input for miles driven
+
+fillUp = prompt ("How many gallons did it take to fill your tank?"); //prompt for number of gallons it took to fill tank
+
+// re-var, meaning my vars to shorter vars for calculations
+
+var V = vehicleType;       //Vehicle type is now V
+
+var SG = startingGallons; //Starting Gallons is now SG
+
+var MD = milesDriven;    //Miles driven is now MD
+alert(MD);
+var FU = fillUp;         //fillUp is now FU
+alert(FU);
 
 // Calculations for MPG
+
+var MPG = (SG - FU) / MD;
 
 
 
 //Your MPG is:
 
-//Display interim fueling to get fuel left in tank
+
+
+
 
 //Distance driven after fueling
 
 //Fuel left in tank of...Vehicle
 
-//You have
+//You have this much fuel left in your tank
+
 //Mathematical part of the program
 
 
