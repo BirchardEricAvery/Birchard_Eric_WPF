f73758a31aa2b1572c75fc3194e3cbaeb8b3901a
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-11 17:51:54 -0700

    Stump coding done

---
+++ Birchard_Eirc_Expressions_Assignment/js/cal.js
@@ -0,0 +1,50 @@
+/**
+ Name:       Eric (Avery) Birchard
+ Assignment: Expressions I
+ Date:       3/11/15
+ Class:      SDI 1503
+ Term:       1
+ */
+
+//alert("test"); First js file test
+
+//Fuel mileage calculator and gas left in tank
+
+//variables
+
+//TODO: declare variables
+
+var startingGallons      //number variable
+var vehicleType          //sting variable
+var isThereGasLeftOver   //boolean variable
+//2 examples of arithmetic operators + and -, /, *
+//example of an array used as part of the calculation
+//string variable as output
+//2 assignment operators
+
+
+
+//TODO: prompts
+//data types number, string, boolean, array
+startingGallons = prompt("Starting fuel in tank"); //number input from user
+
+vehicleType = prompt("What type of vehicle do you drive?"); //string input from user
+isThereGasLeftOver = true; //boolean input from user
+//array with variables for console concatenated with...
+//casting
+//parenthesis for order of operations
+//concatenations
+//outputs
+
+//Concatenations
+
+//TODO: console logs //with descriptive sentences
+//put in results in multi-lined comment /* */
+
+//Save PDF flowchart as Birchard_Eric_Expressions_Flowchart.pdf in Expressions Assignment folder
+
+//make a zip file of Birchard_Eric_Expressions_Assignment.zip for backup on FSO
+
+//Make sure code is in the GitHub Repository
+
+
