ddd709ee0455cad5832d89dc6d1ec306aec55caf
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-15 22:00:09 -0700

    Else if

--- Conditionals homework March 2015/js/Conditionals.js
+++ Conditionals homework March 2015/js/Conditionals.js
@@ -9,18 +9,23 @@
 != not equal to
  = assignment
 == equal to comparing
-
+// if, else two blocks of code...else if multiple blocks
  */
 var kidHeight = 42;
 var minHeight = 48;
-
+var wParentHeight = 45; //the height of the kid with parent
 
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
 if(kidHeight > minHeight) {
-    //code performed if condition is false
+    //you can ride!
     console.log("You can ride the coaster!");
-} else{
-    console.log("Sorry kid you've got some growing to do first!")
+} else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present!");
+}else{
+    //sorry you have growing to do
+    console.log("Sorry kid you've got some growing to do first!");
+
 }
