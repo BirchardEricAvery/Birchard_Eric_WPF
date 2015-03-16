fba5a7bf5ed7d4dff3482bfcad5652d3813eccca
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-15 20:56:46 -0700

    Go to the beach
    else statement introduced

---
+++ Conditionals homework March 2015/js/Conditionals.js
@@ -0,0 +1,13 @@
+/*  Name:       Eric (Avery) Birchard
+    Assignment: Conditionals homework
+    Date:       15, March 2015
+    Class:      SDI
+    Term:       I
+ */
+
+
+sunny = true;
+if(sunny){
+    goToTheBeach();
+}else{goToTheMovies();
+}
