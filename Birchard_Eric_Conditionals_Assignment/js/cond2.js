5a8e107de17768dd95e4a7f66820cbaa1a5f6f27
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-18 11:52:37 -0700

    Stump coding complete

---
+++ Birchard_Eric_Conditionals_Assignment/js/cond2.js
@@ -0,0 +1,138 @@
+/**
+ * Created by avery on 3/18/15.
+ */
+/*
+Name:       Eric (Avery) Birchard
+Assignment: Conditionals 2
+Date:       18, March 2015
+Class:      SDI
+Term:       I
+ */
+
+// idea?
+
+//2 different arithmetic operators + - * /
+
+//2 conditional statements - with an else if or more
+
+//1 ternary conditional statement
+
+//1 logical operator && || !
+
+//3 prompts with validation
+//validation college blank, prompt saying you forgot something, collect that info and put into a variable
+
+
+//concatenations
+
+//console.logs
+
+//multi-lined comment of test value results, eg typed in 5 for length and my calculation gave 30 for the area
+
+
+
+//flow chart to PDF
+//compress Birchard_Eric_Conditionals_Assignment.zip to FSO
+
+//notes
+
+/*      Comparisons
+ >, <, >=, <=
+ != not equal to
+ = assignment
+ == equal to comparing
+ // if, else two blocks of code...else if multiple blocks
+
+
+ Equality Operators/ pairs
+ ==  Equality 6 "6"          True
+ === Strict Equality 6 "6"   False
+ !=  Inequality a != b       True
+ Logical Operators
+
+
+ Compare pairs (boolean)/ relational
+ &&  And             between expressions, boolean, not just a variable
+ a === b && b === c
+ ||  Or (double pipe)
+ ^   Exclusive Or (XOR)  one or the other, not both
+ !   Not
+
+ Ternaries
+ ?       Return a value
+ */
+/*var kidHeight = 42;
+ var minHeight = 48;
+ var wParentHeight = 45; //the height of the kid with parent
+
+
+ //if the child is old enough, print to the console "you can ride!"
+ //if the kid is over 48 inches in height
+ if(kidHeight > minHeight) {
+ //you can ride!
+ console.log("You can ride the coaster!");
+ } else if(kidHeight > wParentHeight){
+ //you can ride with a parent present
+ console.log("You can ride, but only with a parent present!");
+ }else{
+ //sorry you have growing to do
+ console.log("Sorry kid you've got some growing to do first!");
+
+ }
+ var budget = 300;
+ var iPhonePrice = 199.99;
+ var wonLottery = true;
+
+ //if the price of the phone is less our budget AND if our paycheck is over 300
+ if(iPhonePrice < budget || wonLottery === true){
+ console.log("We can buy the phone!");
+ }else{
+ console.log("No phone for you!!");
+ }*/
+
+//Conditional Logic - Ternary Operators
+
+//var gpa = 48;
+
+//if the gpa the min 2.0 score, the student can graduate
+
+/*if(gpa > 2.0){
+ console.log("You can graduate!")
+ }else{
+ console.log("GPA is too low!")
+ }
+ // (gpa > 2.0) ? console.log("You can graduate!"):console.log("GPA too low!");
+ var age = 11;
+ var book;
+ //If the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+
+ book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+
+ console.log("book");
+ */
+/*var age = prompt("Enter age:", "");
+ var isOverSixty = parseInt(age) > 60;
+ console.log("Older than 60: " + isOverSixty);
+
+
+switch (secretNumber)
+    case 1:
+    case 2:
+    case 3:
+        console.log("You guessed the secret number!");
+    case 4:
+    case 5:
+        console.log("Too high!");
+        break;
+    default:
+        console.log("You did not enter a number between 1 and 5.");
+
+        var loopCounter;
+        for (loopCounter =1; loopCounter <= 3; loopCounter++)
+        {
+            //execute this code
+        }
+
+*/
+
+//alert("test");
