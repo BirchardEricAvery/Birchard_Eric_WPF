53c4ef935175debac128e5821b13ff03ccc431ce
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-22 00:24:22 -0700

    function w * h

--- Functions/js/funct.js
+++ Functions/js/funct.js
@@ -14,19 +14,29 @@ function functionName(){
     code the function runs
 }
 
- */
+
 //Reusable code
 function outptMsg(){
     console.log("Hello Professor Lee");
 }
 
+var width = 5;
+
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+    //console.log(area);
 }
+console.log(width);
 //invocation of our function, function call
 calcArea();
-calcArea();
-calcArea();
+
+//arguments --
+//parameters -- solution
+
+ */
+calcArea(30, 20);
+function calcArea(w, h){ //w=30, h=20
+    var area = w * h;
+    console.log(area);
