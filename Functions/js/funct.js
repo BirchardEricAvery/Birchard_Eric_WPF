1afc98a81dbc93ba662066de8445305e28135502
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-22 00:10:18 -0700

    Function call

--- Functions/js/funct.js
+++ Functions/js/funct.js
@@ -1,5 +1,32 @@
-/**
- * Created by avery on 3/21/15.
+/*
+
+ Name:       Eric (Avery) Birchard
+ Assignment: Functions homework
+ Date:       21 - , March 2015
+ Class:      SDI
+ Term:       I
+
  */
 
-alert("test");
+//basic layout
+/*
+function functionName(){
+    code the function runs
+}
+
+ */
+//Reusable code
+function outptMsg(){
+    console.log("Hello Professor Lee");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+//invocation of our function, function call
+calcArea();
+calcArea();
+calcArea();
