766ea1f91d704e7950a4d70dc3c49899d6a93af9
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-22 21:41:53 -0700

    Return call
    return puts a variable into an out put, which can then be referenced
    via another variable that is assigned to a function.
    
    We can then reference the function outside of the brackets via the
    assigned variable

--- Functions/js/funct.js
+++ Functions/js/funct.js
@@ -6,11 +6,9 @@
 */
 /*var width = 5;
 
-function calcArea(){
-    var width = 20;
-    var height = 30;
-    var area = width * height;
-    //console.log(area);
+
+
+
 }
 console.log(width);
 // calcArea(); invocation of our function, function call
@@ -24,6 +22,7 @@ console.log(width);
 
 
 // calcArea(30, 20);           // arguments stored arguments
+/*
 function dogYears(age) {        //parameters are the bins, holds
 
 
@@ -34,3 +33,12 @@ function dogYears(age) {        //parameters are the bins, holds
 var age1 = 4;
     dogYears(age1); //4 year old dog, Call, passing in 4 or age1    Argument
     dogYears(5); //5 year old dog, passing in 5 or age2             Argument
+    */
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h) {
+    var area = w * h;
+    return area; //function spitting the info out
+}
+console.log(total);
