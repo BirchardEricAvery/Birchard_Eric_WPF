67d5a1e291ea0a1b16c91213987c5606ae1f5045
BirchardEricAvery <eabirchard@fullsail.edu>
2015-03-24 00:53:19 -0700

    While loops

---
+++ Loops/js/loop.js
@@ -0,0 +1,17 @@
+/*  Name:       Eric Avery Birchard
+ Assignment: Loops
+ Date:       24, March 2015
+ Class:      SDI
+ Term:       I
+ */
+
+//while loop
+
+console.log('------ Loops ------');
+
+var b = 50;             //sets up the index
+
+while (b > 0){          //checks the condition
+    console.log(b + ' kegs on the wall');
+    b--;                //increments or decrements the index
+}
